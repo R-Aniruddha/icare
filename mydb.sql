d796956e0629d1a6b4e7e09fa4e6b4caee0614ad
R-Aniruddha <aniruddha04@gmail.com>
2019-12-07 00:35:32 +0100

    commit on 'master'

--- mydb.sql
+++ mydb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2019 at 10:31 AM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.11
+-- Generation Time: Dec 07, 2019 at 12:32 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,11 +30,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `idAdmin` int(11) NOT NULL,
-  `Name` varchar(45) DEFAULT NULL,
+  `Username` varchar(45) DEFAULT NULL,
+  `Password` varchar(20) NOT NULL,
   `Adress` varchar(45) DEFAULT NULL,
   `Mobile_number` int(11) DEFAULT NULL,
   `Emergency_contact` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`idAdmin`, `Username`, `Password`, `Adress`, `Mobile_number`, `Emergency_contact`) VALUES
+(1, 'administrator', 'pass', 'Paris', 987654321, 123456789);
 
 -- --------------------------------------------------------
 
@@ -49,6 +57,37 @@ CREATE TABLE `doctor` (
   `Mobile_number` varchar(45) DEFAULT NULL,
   `Emergency_contact` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctorsdetails`
+--
+
+CREATE TABLE `doctorsdetails` (
+  `Name` varchar(100) NOT NULL,
+  `Phone` varchar(100) NOT NULL,
+  `Street` varchar(100) NOT NULL,
+  `City` varchar(100) NOT NULL,
+  `Country` varchar(50) NOT NULL,
+  `Website` varchar(200) NOT NULL,
+  `gen` varchar(100) NOT NULL,
+  `dob` varchar(100) NOT NULL,
+  `Nationality` varchar(100) NOT NULL,
+  `Speciality` varchar(100) NOT NULL,
+  `Department` varchar(100) NOT NULL,
+  `Qualifications` varchar(100) NOT NULL,
+  `Availability` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctorsdetails`
+--
+
+INSERT INTO `doctorsdetails` (`Name`, `Phone`, `Street`, `City`, `Country`, `Website`, `gen`, `dob`, `Nationality`, `Speciality`, `Department`, `Qualifications`, `Availability`) VALUES
+('rakesh', '56885444', 'kmm', 'kmm', 'india', 'http://rakesh.hh', 'male', '1998-01-07', 'indian', 'nothing', 'cse', 'hhhh', 'yes'),
+('Doctor1', '1234657980', 'Rue ', 'Paris', 'France', 'www.doctor.com', 'male', '2019-06-18', 'French', 'Surgeon', 'Surgery', 'MBBS', 'yes'),
+('Doctor1', '1234657980', 'Rue ', 'Paris', 'France', 'www.doctor.com', 'male', '2019-06-18', 'French', 'Surgeon', 'Surgery', 'MBBS', 'yes');
 
 -- --------------------------------------------------------
 
@@ -122,6 +161,27 @@ CREATE TABLE `sensordata` (
   `Motionsensordata` varchar(45) DEFAULT NULL,
   `PatientId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `user_type` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `user_type`, `password`) VALUES
+(0, 'Aniruddha', 'aniruddha04@gmail.com', '', '1a1dc91c907325c69271ddf0c944bc72');
 
 --
 -- Indexes for dumped tables
