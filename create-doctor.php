e47c93cc06a0a401dbc89ed17c0f249f63338459
R-Aniruddha <aniruddha04@gmail.com>
2019-12-10 09:07:33 +0100

    commit on 'master'

--- create-doctor.php
+++ create-doctor.php
@@ -7,33 +7,33 @@
 
         
         
-        <form method="post" action="create-doctor.php" style="padding: 20px;">
+        <form id="create-doctor-form" method="post" action="create-doctor.php" style="padding: 20px;">
             
             <fieldset class="col-md-8" style="margin: auto;">
 
-            <?php include('controller/errors.php'); ?>
-                
-                <h2>Admin - Create Doctor Account</h2>
+                <?php include('controller/errors.php'); ?>
+                    
+                    <h2>Admin - Create Doctor Account</h2>
 
-            <div class="input-group">
-                <label class="input-group-text">Username</label>
-                <input type="text" class="form-control" name="username" value="<?php echo $username; ?>">
-            </div>
-            <div class="input-group">
-                <label class="input-group-text">Email</label>
-                <input type="email" class="form-control" name="email" value="<?php echo $email; ?>">
-            </div>
-            <div class="input-group">
-                <label class="input-group-text">Password</label>
-                <input type="password" name="password_1" class="form-control">
-            </div>
-            <div class="input-group">
-                <label class="input-group-text">Confirm password</label>
-                <input type="password" name="password_2" class="form-control">
-            </div>
-            <div class="input-group">
-                <button type="submit" class="btn btn-primary btn-md" name="create_temp_doc_ac">Create Account</button>
-            </div>
+                <div class="input-group">
+                    <label class="input-group-text">Username</label>
+                    <input type="text" class="form-control" name="username" value="<?php echo $username; ?>">
+                </div>
+                <div class="input-group">
+                    <label class="input-group-text">Email</label>
+                    <input type="email" class="form-control" name="email" value="<?php echo $email; ?>">
+                </div>
+                <div class="input-group">
+                    <label class="input-group-text">Password</label>
+                    <input type="password" name="password_1" class="form-control">
+                </div>
+                <div class="input-group">
+                    <label class="input-group-text">Confirm password</label>
+                    <input type="password" name="password_2" class="form-control">
+                </div>
+                <div class="input-group">
+                    <button type="submit" class="btn btn-primary btn-md" name="create_temp_doc_ac">Create Account</button>
+                </div>
             </fieldset>    
         </form>
           
