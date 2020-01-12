b5e01921101b0019b9d502f6441e4fe4c0277982
R-Aniruddha <aniruddha04@gmail.com>
2020-01-12 15:38:36 +0100

    commit on 'master'

--- doctor-dashboard.php
+++ doctor-dashboard.php
@@ -29,29 +29,44 @@
         <?php endif ?> </h2>   
         <br>
         <h3 class="text-center">Your Patients</h3>
-
-        <div class="card-container row">
-            <?php 
-                $results = mysqli_query($conn, "SELECT * FROM patient WHERE DoctorId = '$docid'"); 
-
-                while ($row = mysqli_fetch_assoc($results)) { 
-                    $str = strtok($row['DoctorComments'], "\n");
-                    ?>
-
-                <div class="card col-md-3" style="margin: 20px;">
-                    <img class="card-img-top" src="images/avatar1.png" alt="Patient Image" style="padding: 20px;">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $row['First_Name'] ," ", $row['Last_Name']; ?></h5>
-                        <h6><strong><?php if($row['RoomNo'] !=0 )echo "Room Number: ",$row['RoomNo'] ?> </strong> </h6>
-                        <h6><strong><?php if($row['RoomNo'] == 0 )echo "Room Not Assigned" ?> </strong></h6>
-                        <p class="card-text"><?php echo $str , "..."?></p>
-                        <a href="view-patient-dashboard.php?id=<?php echo $row['idPatient']?>" class="btn btn-info btn-sm">View Patient Dashboard</a>
-                    </div>
+        <div class="container-fluid">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
+                <div class="carousel-inner row w-100 mx-auto">
+                    <?php 
+                        $results = mysqli_query($conn, "SELECT * FROM patient WHERE DoctorId = '$docid'"); 
+                        while ($row = mysqli_fetch_assoc($results)) { 
+                            $str = strtok($row['DoctorComments'], "\n");
+                        ?>
+                        
+                        <div class="carousel-item col-md-4">
+                            <div class="card" style="margin: 20px;">
+                                <img class="card-img-top" src="images/avatar1.png" alt="Patient Image" style="padding: 20px;">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo $row['First_Name'] ," ", $row['Last_Name']; ?></h5>
+                                    <h6><strong><?php if($row['RoomNo'] !=0 )echo "Room Number: ",$row['RoomNo'] ?> </strong> </h6>
+                                    <h6><strong><?php if($row['RoomNo'] == 0 )echo "Room Not Assigned" ?> </strong></h6>
+                                    <p class="card-text"><?php echo $str , "..."?></p>
+                                    <a href="view-patient-dashboard.php?id=<?php echo $row['idPatient']?>" class="btn btn-info btn-sm">View Patient Dashboard</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
                 </div>
-
-            <?php } ?>
+                <!--Controls-->
+                <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+                <!--/.Controls-->
+            </div>
+                <!--/.Inner-->
 
         </div>
+        
                 
 
 
--- js/script.js
+++ js/script.js
@@ -2,12 +2,25 @@ $(document).ready(function() {
   $("#myCarousel .carousel-item").first().addClass("active");
   $("#myCarousel2 .carousel-item").first().addClass("active");
 
+  var num1 = $("#myCarousel .carousel-item").length;
+  var num2 = $("#myCarousel2 .carousel-item").length;
+    if(num1<4) {
+      document.getElementsByClassName("carousel-control-next")[0].style.display = "none";
+      document.getElementsByClassName("carousel-control-prev")[0].style.display = "none";
+    }
+    if(num2<4) {
+      document.getElementsByClassName("carousel-control-next")[1].style.display = "none";
+      document.getElementsByClassName("carousel-control-prev")[1].style.display = "none";
+    }
+
+
   $("#myCarousel").on("slide.bs.carousel", function(e) {
     //alert("Button clicked");
     var $e = $(e.relatedTarget);
     var idx = $e.index();
     var itemsPerSlide = 3;
     var totalItems = $("#myCarousel .carousel-item").length;
+    
     //alert(totalItems);
 
     if (idx >= totalItems - (itemsPerSlide - 1)) {
